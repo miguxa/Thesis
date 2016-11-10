6ce3318b868063ac796121b144aabac288f4d344
Miguel Prego <miguel_prego1992@hotmail.com>
2016-11-10 17:02:25 +0000

    Adicionar actualização de lista

--- Android/OtherTest/app/src/main/java/com/example/miguel/othertest/MainActivity.java
+++ Android/OtherTest/app/src/main/java/com/example/miguel/othertest/MainActivity.java
@@ -2,17 +2,18 @@ package com.example.miguel.othertest;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
+import android.view.View.OnClickListener;
+import android.bluetooth.BluetoothDevice;
 
 import java.util.ArrayList;
 import java.util.Set;
@@ -28,13 +29,26 @@ public class MainActivity extends Activity {
         super.onCreate(icicle);
         setContentView(R.layout.activity_main);
 
-        final Button button =(Button) findViewById(R.id.button3);
+        final Button button1 =(Button) findViewById(R.id.button1);
+        final Button button2 =(Button) findViewById(R.id.button2);
+        final Button button3 =(Button) findViewById(R.id.button3);
+        final Button button4 =(Button) findViewById(R.id.button4);
         BA = BluetoothAdapter.getDefaultAdapter();
         lv = (ListView)findViewById(R.id.list1);
         ArrayList listaAux = new ArrayList();
         emparelhados = BA.getBondedDevices();
 
-        button.setOnClickListener(new View.OnClickListener() {
+        if (BA == null) {
+            Toast.makeText(getApplicationContext(), "Bluetooth não disponivel", Toast.LENGTH_LONG);
+            finish();
+        }
+        else
+            if (!BA.isEnabled()) {
+                Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(turnOn, 0);
+            }
+
+        button3.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 TextView view = (TextView) findViewById(R.id.textView1);
                 if (aux%2 == 0)
@@ -47,9 +61,15 @@ public class MainActivity extends Activity {
 
         for (BluetoothDevice bt : emparelhados)
             listaAux.add(bt.getName());
-
         final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listaAux);
         lv.setAdapter(adapter);
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                pairedDevices();
+            }
+        });
+
     }
 
     public void on (View v) {
