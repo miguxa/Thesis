9738f676cafd77e044e8e49f08e446bd01b3ea52
Miguel Prego <miguel_prego1992@hotmail.com>
2016-11-30 17:16:36 +0000

    Acelerómetro a recolher valores certos

--- SparkFun_ADXL345_Arduino_Library/examples/SparkFun_ADXL345_Example/SparkFun_ADXL345_Example.ino
+++ SparkFun_ADXL345_Arduino_Library/examples/SparkFun_ADXL345_Example/SparkFun_ADXL345_Example.ino
@@ -1,73 +1,35 @@
-/*  ********************************************* 
- *  SparkFun_ADXL345_Example
- *  Triple Axis Accelerometer Breakout - ADXL345 
- *  Hook Up Guide Example 
- *  
- *  Utilizing Sparkfun's ADXL345 Library
- *  Bildr ADXL345 source file modified to support 
- *  both I2C and SPI Communication
- *  
- *  E.Robert @ SparkFun Electronics
- *  Created: Jul 13, 2016
- *  Updated: Sep 06, 2016
- *  
- *  Development Environment Specifics:
- *  Arduino 1.6.11
- *  
- *  Hardware Specifications:
- *  SparkFun ADXL345
- *  Arduino Uno
- *  *********************************************/
-
 #include <SparkFun_ADXL345.h>         // SparkFun ADXL345 Library
 
-/*********** COMMUNICATION SELECTION ***********/
-/*    Comment Out The One You Are Not Using    */
-//ADXL345 adxl = ADXL345(10);           // USE FOR SPI COMMUNICATION, ADXL345(CS_PIN);
 ADXL345 adxl = ADXL345();             // USE FOR I2C COMMUNICATION
-int timer=0;
+int tmr=0;
 
-/****************** INTERRUPT ******************/
-/*      Uncomment If Attaching Interrupt       */
-//int interruptPin = 2;                 // Setup pin 2 to be the interrupt pin (for most Arduino Boards)
-
-
-/******************** SETUP ********************/
-/*          Configure ADXL345 Settings         */
 void setup(){
 
   Serial.begin(9600);                 // Start the serial terminal
-  Serial.println("");
   Serial.println("SparkFun ADXL345 Accelerometer Hook Up Guide Example");
   Serial.println();
   pinMode(2, INPUT);
   
   adxl.powerOn();                     // Power on the ADXL345
 
-  adxl.setRangeSetting(2);           // Give the range settings
+  adxl.setRangeSetting(2);            // Give the range settings
                                       // Accepted values are 2g, 4g, 8g or 16g
                                       // Higher Values = Wider Measurement Range
                                       // Lower Values = Greater Sensitivity
-
 }
 
-/****************** MAIN CODE ******************/
-/*     Accelerometer Readings and Interrupt    */
 void loop(){
   // Accelerometer Readings
   int x,y,z,i;
-  int S[400];
+  int S[450];
 
   Serial.println("Carregar para iniciar");
-  
   while(digitalRead(2) == LOW)
     ;
-
   Serial.println("Inicio");
   
-  for(i=0; i<400; i=i+3) {
+  for(i=0; i<450; i=i+3) {
     adxl.readAccel(&x, &y, &z);         // Read the accelerometer values and store them in variables declared above x,y,z
-    timer = timer + 1;
     S[i+0]=x;
     S[i+1]=y;
     S[i+2]=z;
@@ -76,22 +38,15 @@ void loop(){
 
   Serial.println("Fim");
   delay(1000);
+  Serial.println("Time    X    Y    Z");
   
-  for(i=0; i<400; i=i+3) {
+  for(i=0; i<450; i=i+3) {
+    PrintTimer();
     PrintSinal(S[i+0]);
     PrintSinal(S[i+1]);
     PrintSinal(S[i+2]);
     Serial.println("");
   }
-  // Output Results to Serial
-  /* UNCOMMENT TO VIEW X Y Z ACCELEROMETER VALUES */  
-  /*Serial.print(x);
-  Serial.print(", ");
-  Serial.print(y);
-  Serial.print(", ");
-  Serial.println(z); 
-  */
-  //delay(50);
 }
 
 void PrintSinal (int val) {
@@ -110,5 +65,19 @@ void PrintSinal (int val) {
   if (val < 0 && val > -100)
     val = val * -1;
   Serial.print(val);
+  Serial.print(" ");
+}
+
+void PrintTimer () {
+  tmr = tmr + 1;
+  if (tmr < 10)
+    Serial.print("0000");
+  else if (tmr < 100)
+    Serial.print("000");
+  else if (tmr < 1000)
+    Serial.print("00");
+  else if (tmr < 10000)
+    Serial.print("0");
+  Serial.print(tmr);
   Serial.print(" ");
 }
